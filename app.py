4f630aba22d341c8b2bada6e85c34a12b926c8d2
dperezflores <daniel.apf99@gmail.com>
2026-05-21 01:07:15 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+from ui import renderizar_interfaz
+
+if __name__ == "__main__":
+    # Este es el punto de entrada principal de la aplicación.
+    # Solo se encarga de llamar a la función que dibuja la interfaz.
+    renderizar_interfaz()
